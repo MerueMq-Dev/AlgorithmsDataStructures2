a865ef077e3d6d2c8ad45631ecf0433a168cbc51
LAPTOP\work <ichiru98@mail.ru>
2024-12-13 21:15:51 +0300

    clean

--- BST.cs
+++ BST.cs
@@ -535,9 +535,7 @@ namespace AlgorithmsDataStructures2
             
             return maxLevelLeft > maxLevelRight ? maxLevelLeft : maxLevelRight;
         }
-
-        // префиксный массив: [1,2,4,5,3,6,7]
-        // инфиксный массив: [4,2,5,1,6,3,7]
+        
         public void RestoreTree(int[] prefixTraversal,int[] infixTraversal)
         {
             Root = RestoreTree(prefixTraversal, infixTraversal, 0, 0);
