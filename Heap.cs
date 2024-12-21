9ea5fbb52a821ebdbc29be9145414dc8cb73bcc6
LAPTOP\work <ichiru98@mail.ru>
2024-12-21 23:15:02 +0300

    mergeHeaps

--- Heap.cs
+++ Heap.cs
@@ -12,11 +12,13 @@ namespace AlgorithmsDataStructures2
             HeapArray = null;
         }
 
+        public int CurrentSize => HeapArray.Length;
+
         public void MakeHeap(int[] keys, int depth)
         {
             int heapSize = depth <= 0 ? 1 : (int)Math.Pow(depth + 1, 2) - 1;
             HeapArray = new int[heapSize];
-            
+
             foreach (var key in keys)
             {
                 bool isAdded = Add(key);
@@ -25,39 +27,40 @@ namespace AlgorithmsDataStructures2
             }
         }
 
+
         public int GetMax()
         {
             if (HeapArray == null || HeapArray.Length == 0)
                 return -1;
-            
+
             int max = HeapArray[0];
             if (max == 0)
                 return -1;
-            
-            HeapArray[0] = HeapArray[HeapArray.Length - 1];  
-            
+
+            HeapArray[0] = HeapArray[HeapArray.Length - 1];
+
             HeapArray[HeapArray.Length - 1] = 0;
-            
-            int heapSize = HeapArray.Length - 1;  
-            for (int currentIndex = 0;true;)
+
+            int heapSize = HeapArray.Length - 1;
+            for (int currentIndex = 0;;)
             {
                 int leftChildIndex = currentIndex * 2 + 1;
                 int rightChildIndex = currentIndex * 2 + 2;
                 int largestIndex = currentIndex;
-                
+
                 if (leftChildIndex < heapSize && HeapArray[leftChildIndex] > HeapArray[largestIndex])
                 {
                     largestIndex = leftChildIndex;
                 }
-                
+
                 if (rightChildIndex < heapSize && HeapArray[rightChildIndex] > HeapArray[largestIndex])
                 {
                     largestIndex = rightChildIndex;
                 }
-                
+
                 if (largestIndex == currentIndex)
                     break;
-                
+
                 (HeapArray[currentIndex], HeapArray[largestIndex]) =
                     (HeapArray[largestIndex], HeapArray[currentIndex]);
                 currentIndex = largestIndex;
@@ -66,7 +69,99 @@ namespace AlgorithmsDataStructures2
             return max;
         }
 
+        public int FindMaxInRange(int low, int high)
+        {
+            int max = -1;
+            foreach (var value in HeapArray)
+            {
+                if (value >= low && value <= high && max < value)
+                {
+                    max = value;
+                }
+            }
 
+            return max;
+        }
+
+        public int Find(Predicate<int> predicate)
+        {
+            int currentIndex = 0;
+            if (HeapArray == null)
+                return -1;
+
+            return Find(predicate, currentIndex);
+        }
+
+        private int Find(Predicate<int> predicate, int currentIndex)
+        {
+            if (currentIndex >= HeapArray.Length)
+                return -1;
+
+            if (predicate(HeapArray[currentIndex]))
+            {
+                return HeapArray[currentIndex];
+            }
+
+            int leftResult = -1;
+            int leftIndex = currentIndex * 2 + 1;
+            if (leftIndex < HeapArray.Length && HeapArray[leftIndex] <= HeapArray[currentIndex])
+            {
+                leftResult = Find(predicate, leftIndex);
+            }
+
+            if (leftResult != -1)
+                return leftResult;
+
+            int rightIndex = currentIndex * 2 + 2;
+            int rightResult = -1;
+            if (rightIndex < HeapArray.Length && HeapArray[rightIndex] <= HeapArray[currentIndex])
+            {
+                rightResult = Find(predicate, rightIndex);
+            }
+
+            return rightResult;
+        }
+
+        public bool MergeHeaps(Heap heapToMerge)
+        {
+            if (HeapArray == null)
+                return false;
+
+            if (heapToMerge == null || heapToMerge.CurrentSize == 0)
+                return true;
+
+            int[] mergedArray = new int[CurrentSize + heapToMerge.CurrentSize];
+            int maxValue = heapToMerge.GetMax();
+            int currentIndex = 0;
+            while (maxValue != -1)
+            {
+                mergedArray[currentIndex] = maxValue;
+                currentIndex++;
+                maxValue = heapToMerge.GetMax();
+            }
+
+            maxValue = GetMax();
+            while (maxValue != -1)
+            {
+                mergedArray[currentIndex] = maxValue;
+                currentIndex++;
+                maxValue = GetMax();
+            }
+
+            int newHeapDepth = CalculateDepth(mergedArray.Length);
+            MakeHeap(mergedArray, newHeapDepth);
+            return true;
+        }
+
+
+        public int CalculateDepth(int heapSize)
+        {
+            if (heapSize <= 0)
+                return 0; // Невозможно вычислить глубину для некорректного размера кучи
+
+            int depth = (int)(Math.Sqrt(heapSize + 1) - 1);
+            return depth;
+        }
 
         public bool IsHeap()
         {
@@ -119,7 +214,7 @@ namespace AlgorithmsDataStructures2
             for (int currentIndex = smallestElementIndex; currentIndex > 0;)
             {
                 int parentIndex = (currentIndex - 1) / 2;
-                
+
                 if (HeapArray[parentIndex] < HeapArray[currentIndex])
                 {
                     (HeapArray[currentIndex], HeapArray[parentIndex]) =
--- Program.cs
+++ Program.cs
@@ -13,88 +13,70 @@ namespace AlgorithmsDataStructures2
             int test = Array.IndexOf(s, null);
             Console.WriteLine($"test {test}");
 
-            int[] keys = new[] { 8, 2, 1 };
+            int[] keys = new[] { 8, 3, 1, 4, 5, 2 };
             Heap heap = new Heap();
-            heap.MakeHeap(keys, 1);
+            heap.MakeHeap(keys, 3);
+            
+            Console.WriteLine(string.Join(", ", heap.HeapArray));   
+            
+            int[] keys2 = new[] { 8, 3, 1 };
+            Heap heap2 = new Heap();
+            heap2.MakeHeap(keys2, 2);
+
+            heap.MergeHeaps(heap2);
+            
             Console.WriteLine($"IsHeap: {heap.IsHeap()}");
-
-            Console.WriteLine(string.Join(", ", heap.HeapArray).Replace("-1", "_"));
-            int max = heap.GetMax();
-
-            Console.WriteLine(max);
-            Console.WriteLine(string.Join(", ", heap.HeapArray).Replace("-1", "_"));
-
-            int max2 = heap.GetMax();
-            Console.WriteLine(string.Join(", ", heap.HeapArray).Replace("-1", "_"));
-            Console.WriteLine(max2);
-
-            int max3 = heap.GetMax();
-            Console.WriteLine(string.Join(", ", heap.HeapArray).Replace("-1", "_"));
-            Console.WriteLine(max3);
-
-            int max4 = heap.GetMax();
-            Console.WriteLine(string.Join(", ", heap.HeapArray).Replace("-1", "_"));
-            Console.WriteLine(max4);
-
+            Console.WriteLine(string.Join(", ", heap.HeapArray));
             Console.WriteLine();
             // heap.HeapArray[1] = 1;
 
-            Console.WriteLine($"IsHeap: {heap.IsHeap()}");
-
-            // 0 - 1
-            // 1 - 3
-            // 2 - 7
-            // 3 - 15
-
-            SimpleGraph simpleGraph = new SimpleGraph(4);
-            simpleGraph.AddVertex(1);
-            simpleGraph.AddVertex(2);
-            simpleGraph.AddVertex(3);
-            simpleGraph.AddVertex(4);
-
-            simpleGraph.AddEdge(1, 2);
-            simpleGraph.AddEdge(0, 3);
-            simpleGraph.AddEdge(3, 1);
-            simpleGraph.AddEdge(2, 0);
-
-            Console.WriteLine("BEFORE");
-            
-            int rows = simpleGraph.m_adjacency.GetUpperBound(0) + 1; // количество строк
-            int columns = simpleGraph.m_adjacency.Length / rows;
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    Console.Write($"{simpleGraph.m_adjacency[i, j]} ");
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.WriteLine();
-            
-            simpleGraph.RemoveVertex(2);
-            Console.WriteLine("AFTER"); 
-            
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    Console.Write($"{simpleGraph.m_adjacency[i, j]} ");
-                }
-
-                Console.WriteLine();
-            }
-            
-            bool isEdge = simpleGraph.IsEdge(1, 2);
-            Console.WriteLine($"IsEdge: {isEdge}");
-
-
+            // SimpleGraph simpleGraph = new SimpleGraph(4);
+            // simpleGraph.AddVertex(1);
+            // simpleGraph.AddVertex(2);
+            // simpleGraph.AddVertex(3);
+            // simpleGraph.AddVertex(4);
+            //
+            // simpleGraph.AddEdge(1, 2);
+            // simpleGraph.AddEdge(0, 3);
+            // simpleGraph.AddEdge(3, 1);
+            // simpleGraph.AddEdge(2, 0);
+            //
+            // Console.WriteLine("BEFORE");
+            //
+            // int rows = simpleGraph.m_adjacency.GetUpperBound(0) + 1; // количество строк
+            // int columns = simpleGraph.m_adjacency.Length / rows;
+            //
+            // for (int i = 0; i < rows; i++)
+            // {
+            //     for (int j = 0; j < columns; j++)
+            //     {
+            //         Console.Write($"{simpleGraph.m_adjacency[i, j]} ");
+            //     }
+            //
+            //     Console.WriteLine();
+            // }
+            //
+            // Console.WriteLine();
+            //
+            // simpleGraph.RemoveVertex(2);
+            // Console.WriteLine("AFTER"); 
+            //
+            // for (int i = 0; i < rows; i++)
+            // {
+            //     for (int j = 0; j < columns; j++)
+            //     {
+            //         Console.Write($"{simpleGraph.m_adjacency[i, j]} ");
+            //     }
+            //
+            //     Console.WriteLine();
+            // }
+            //
+            // bool isEdge = simpleGraph.IsEdge(1, 2);
+            // Console.WriteLine($"IsEdge: {isEdge}");
 
 
             Console.WriteLine();
-            
+
             // BSTNode<int> rootNode = new BSTNode<int>(4, 4, null);
             // List<int> values = new List<int> { 2, 1, 3, 6, 5, 7 };
             // BST<int> tree = new BST<int>(null);
